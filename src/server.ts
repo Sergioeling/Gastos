ba5fdc521b797d8abd02baa5237f5944ff702e3a
Sergio Carcaño <101364383+Sergioeling@users.noreply.github.com>
2026-01-29 12:47:17 -0600

    Add server start logic with environment port

--- src/server.ts
+++ src/server.ts
@@ -51,8 +51,12 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-
-
+if (isMainModule(import.meta.url)) {
+  const port = process.env['PORT'] || 4000;
+  app.listen(port, (error) => {
+    if (error) {
+      throw error;
+    }
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
